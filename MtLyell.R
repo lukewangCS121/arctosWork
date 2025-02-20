740f85dc59bd9a8b779b7bd9b93e1b4b6a7615ce
lukewangCS121 <lukejiamingwang@gmail.com>
2025-02-20 10:32:48 -0800

    Starter version of MtLyell

---
+++ MtLyell.R
@@ -0,0 +1,11 @@
+library(ArctosR)
+library(tidyverse)
+library(ggplot2)
+result_params <- get_result_parameters()
+MVZ_sorex<-
+  get_records(scientific_name="Sorex lyelli", guid_prefix="MVZ:Mamm", 
+              columns=list("guid", "year_collected"),all_records = TRUE)
+SorexDF<-response_data(MVZ_sorex)
+ggplot(data=SorexDF, aes(x=guid, y=year_collected))+
+  geom_point()
+
