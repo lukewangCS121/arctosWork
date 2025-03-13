7802abd973ca79dcf30ac6141f20dbb2f1388c67
lukewangCS121 <lukejiamingwang@gmail.com>
2025-03-13 12:11:17 -0700

    Make distinct species into barChart

--- UniqueSpeciesSulawesi.R
+++ UniqueSpeciesSulawesi.R
@@ -6,4 +6,12 @@ MVZ_Sulawesi<-
   get_records(state_prov="Sulawesi", guid_prefix="MVZ:Mamm", 
               columns=list("guid", "species"),all_records = TRUE) #get all specimen from Sulawesi
 SulaDF<-response_data(MVZ_Sulawesi) #form into dataframe
+head(SulaDF)
 uniqueSpeciesSula<-distinct(SulaDF, species) #get all unique species
+ggplot(data=SulaDF, aes(x=factor(species)))+
+  geom_bar()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+#adjust angle of x axis label
+  labs(x="Species Name")
+
+#add frequency into a barchart
+
